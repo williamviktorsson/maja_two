383dc669333d18aaf881cc7fc4377582b957358b
William Viktorsson <wilvik05@edu.umea.se>
2021-11-17 08:39:51 +0100

    added clock and some small examples

--- lesson_1/clock_svelte/src/App.svelte
+++ lesson_1/clock_svelte/src/App.svelte
@@ -1,77 +1,76 @@
 <script>
+    import { Clock } from "./clock";
 
-	let time=1337;
+    let clock = new Clock();
 
-	function tick() {
-		time++;
-	}
+    let students = [1, 2, 3];
 
-	setInterval(tick,1000)
+    let i = 0;
 
+    function tick() {
+        clock.tick();
+        clock = clock;
+        i++;
+    }
+
+    setInterval(tick, 1000);
 </script>
 
 <main>
-<h1>
-        Hehej
-    </h1>
+    <h1>Hehej {i}</h1>
+
+    <h2>
+        students: {students}
+    </h2>
+
+    <button on:click={() => (students = [...students, students.length])}
+        >add student</button
+    >
 
     <p>
-        Tid: {time}
+        Tid: {clock.time}
     </p>
 
-	{#if (time>=1350)}
-		    <p>
-        VAKNA!!!
-    </p>
+    {#if clock.time == "13:50"}
+        <p>VAKNA!!!</p>
+    {/if}
 
-	{/if}
+    <button on:click={tick}> HIT ME! </button>
 
-    <button on:click={tick}>
-        HIT ME!
-    </button>
+    <p id="time" />
 
-    <p id="time">
+    <p id="alarm" />
 
-    </p>
+    <p id="test">1231231231!</p>
 
-    <p id="alarm">
-
-    </p>
-
-    <p id="test">
-        1231231231!
-    </p>
-
-    <p class="tjenix">
-        1231231231!
-    </p>
+    <p class="tjenix">1231231231!</p>
 </main>
 
 <style>
-	 main {
-            color: red;
-            background-color: pink;
-        }
+    main {
+        color: red;
+        background-color: pink;
+    }
 
-        h1 {
-            color: greenyellow;
-            text-transform: uppercase;
-            font-weight: bold;
-            font-size: 8em;
-        }
+    h1 {
+        color: greenyellow;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 8em;
+    }
 
-        p {
-            font-size: 2em;
-            color: black;
-        }
+    p {
+        font-size: 2em;
+        color: black;
+    }
 
-        .tjenix {
-            font-size: 10em;
-            color: purple;
-        }
+    .tjenix {
+        font-size: 10em;
+        color: purple;
+    }
 
-        #test {
-            font-size: 4em;
-            color: indianred;
-        }
+    #test {
+        font-size: 4em;
+        color: indianred;
+    }
 </style>
