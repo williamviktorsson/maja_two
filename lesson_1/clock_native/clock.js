2fbf516d94b428c464ad8bb2a8e072c6c41172df
williamv <williamv@cs.umu.se>
2021-11-17 08:08:16 +0100

    added lesson_1

---
+++ lesson_1/clock_native/clock.js
@@ -0,0 +1,21 @@
+
+class Clock {
+
+    constructor(hour = 0, minute = 0) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    tick() {
+        this.minute++;
+        if (this.minute == 60) {
+            this.minute = 0
+            this.hour = (this.hour + 1) % 24;
+        }
+    }
+
+    get time() {
+        return this.hour.toString().padStart(2, '0') + ':' + this.minute.toString().padStart(2, '0')
+    }
+
+}
---
+++ lesson_1/clock_svelte/src/App.svelte
@@ -0,0 +1,77 @@
+<script>
+
+	let time=1337;
+
+	function tick() {
+		time++;
+	}
+
+	setInterval(tick,1000)
+
+</script>
+
+<main>
+<h1>
+        Hehej
+    </h1>
+
+    <p>
+        Tid: {time}
+    </p>
+
+	{#if (time>=1350)}
+		    <p>
+        VAKNA!!!
+    </p>
+
+	{/if}
+
+    <button on:click={tick}>
+        HIT ME!
+    </button>
+
+    <p id="time">
+
+    </p>
+
+    <p id="alarm">
+
+    </p>
+
+    <p id="test">
+        1231231231!
+    </p>
+
+    <p class="tjenix">
+        1231231231!
+    </p>
+</main>
+
+<style>
+	 main {
+            color: red;
+            background-color: pink;
+        }
+
+        h1 {
+            color: greenyellow;
+            text-transform: uppercase;
+            font-weight: bold;
+            font-size: 8em;
+        }
+
+        p {
+            font-size: 2em;
+            color: black;
+        }
+
+        .tjenix {
+            font-size: 10em;
+            color: purple;
+        }
+
+        #test {
+            font-size: 4em;
+            color: indianred;
+        }
+</style>
