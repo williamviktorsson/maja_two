38586cb79f3989cd95e04cc7426738df56dd7475
Magdalena Boström <magdab@cs.umu.se>
2022-06-08 09:14:27 +0200

    did something

--- task_5/search/src/App.svelte
+++ task_5/search/src/App.svelte
@@ -1,63 +1,72 @@
 <script>
-    import Spinner from "./Spinner.svelte";
-    import Results from "./Results.svelte";
-    import Search from "./Search.svelte";
-    import { promise } from "./stores.js";
+  import Spinner from "./Spinner.svelte";
+  import Results from "./Results.svelte";
+  import Search from "./Search.svelte";
+  import { promise } from "./stores.js";
 
 </script>
 
-<main>
+<main class:searched={$promise != undefined}>
+  <Search />
 
-    <Search/>
-
-    {#await $promise}
-        <Spinner />
-    {:then result}
-        <Results json={result} />
-    {:catch error}
-        <p style="color: red">{error.message}</p>
-    {/await}
+  {#await $promise}
+    <Spinner />
+  {:then result}
+    <Results json={result} />
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
 </main>
 
 <style>
-    :global(body) {
-        padding: 0;
-        margin: 0;
-    }
+  :global(body) {
+    padding: 0;
+    margin: 0;
+  }
 
-    main {
-        height: 100vh;
-        width: 100vw;
-        background: linear-gradient(
-            -45deg,
-            #b966522f,
-            #8a65743a,
-            #939ea13d,
-            #ddf5ef3f
-        );
-        background-size: 400% 400%;
-        animation: gradient 15s ease infinite;
-        display: flex;
-        justify-content: start;
-        align-items: center;
-        flex-direction: column;
-        gap: 50px;
-        padding-top: 10%;
-        padding-left: 10%;
-        padding-right: 10%;
-        padding-bottom: 5%;
-        box-sizing: border-box;
-    }
+  main {
+    height: 100vh;
+    width: 100vw;
+    background: linear-gradient(
+      -45deg,
+      #b966522f,
+      #8a65743a,
+      #939ea13d,
+      #ddf5ef3f
+    );
+    background-size: 400% 400%;
+    animation: gradient 15s ease infinite;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    flex-direction: column;
+    gap: 50px;
+    padding-top: 10%;
+    padding-left: 10%;
+    padding-right: 10%;
+    padding-bottom: 5%;
+    box-sizing: border-box;
+  }
 
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+  main.searched {
+    background: linear-gradient(
+      -45deg,
+      #c5968a2f,
+      #c6356f3a,
+      #179dc23d,
+      #1480653f
+    );
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
     }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 </style>
--- task_5/search/src/Search.svelte
+++ task_5/search/src/Search.svelte
@@ -1,56 +1,56 @@
 <script>
-    import { promise } from "./stores.js";
-    let question;
-    let searched = false;
-    async function search() {
-        searched = true;
-        const res = await fetch(
-            `https://demo.dataverse.org/api/search?q=` + question
-        );
-        const json = await res.json();
+  import { promise } from "./stores.js";
+  let question;
+  async function search() {
+    const res = await fetch(
+      `https://demo.dataverse.org/api/search?q=` + question
+    );
+    const json = await res.json();
 
-         if (res.ok) {
-            return json;
-        } else {
-            throw new Error(json);
-        } 
+    if (res.ok) {
+      return json;
+    } else {
+      throw new Error(json);
     }
+  }
+
+  $: searched = $promise != undefined;
+
+  
 </script>
 
-<div class:searched={searched==true}>
-    <form
-        on:submit|preventDefault={() => {
-            $promise = search();
-        }}
-    >
-        <input bind:value={question} />
-    </form>
+<div class:searched>
+  <form
+    on:submit|preventDefault={() => {
+      $promise = search();
+    }}
+  >
+    <input bind:value={question} />
+  </form>
 </div>
 
 <style>
-    div {
-        display: flex;
-        gap: 10px;
-        width: 30%;
-        justify-self: center;
-        align-items: center;
-        transition: all 0.3s;
-        position: fixed;
-        top: 20%;
-        left: 35%;
-    }
+  div {
+    display: flex;
+    gap: 10px;
+    width: 30%;
+    justify-self: center;
+    align-items: center;
+    transition: all 0.3s;
+    position: fixed;
+    top: 20%;
+    left: 35%;
+  }
 
-    div.searched {
- 
-        position: fixed;
-        left: 10%;
-        top: 20%;
-        width: 30%;
+  div.searched {
+    position: fixed;
+    left: 10%;
+    top: 20%;
+    width: 30%;
+  }
 
-    }
-
-    form,
-    form input {
-        width: 100%;
-    }
+  form,
+  form input {
+    width: 100%;
+  }
 </style>
