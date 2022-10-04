a5e77615c372b6443fb1a4908ec369b459c3d28c
williamv <williamv@cs.umu.se>
2022-10-04 19:38:18 +0200

    backup

---
+++ task_7/daisy/src/routes/ville/index@blank.svelte
@@ -0,0 +1,99 @@
+<div class="grid md:grid-cols-3"> <!--contet wrapper-->
+ 
+        <div class="md:col-span-1">
+            <nav>
+                <div>
+                    <h1 class="font-bold uppercase p-4 border-b border-black-300">
+                        <a  href="/" class="text-green-500 sm: text-red-500 lg-text-blue-500" >luleå hockey</a>
+                    </h1>
+                </div>
+                <ul>
+                    <li class="text-green-500 text-xl">
+                        <a href="#">
+                            <span>Home</span>
+                        </a>
+                    </li>
+                    <li class="text-green-500">
+                        <a href="#">
+                            <span>
+                                About
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span>
+                                contact
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <main class="px-16 py-6 bg-gray-100 md:col-span-2">
+            <div class="flex fustify-center md: fustify-end">
+                <a class="text-red-700 rounded-full py-2 px-3 border-primary bordern-2 uppercase text-xs font-bold cursor-pointer tracking-wider" href="/">log in</a>
+                <a class="text-red-700 ml-2 border-primary bordern-2 uppercase text-xs font-bold cursor-pointer tracking-wider" href="/">sign up</a>
+       
+            </div>
+            <div class="flex items-end">
+              <div class="gg-red-500 h-4 w-6">
+              <div class="bg-blue-500 h-8 w-6">
+              <div class="bg-green-500 h-12 w-6">
+ 
+              </div>
+ 
+              </div>
+              </div>
+            </div>
+            <header>
+                <h2>players</h2>
+                <h3>for lulea fans</h3>
+            </header>
+            <div class="mt-8 grid lg:grid-cols-3 gap-10">
+                <h4 class="font-bold mt-12 pb-2 border-b border-black-200">top players</h4>
+                <div class="m-4">
+                    <div class="bg-white rounded overflow-hidden shadow-md relative">
+                        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/95/Lulea_Hockey_logo.svg/300px-Lulea_Hockey_logo.svg.png" alt="" class="w-full h-32 s,: h-48 object-cover">
+                        <div>
+                            <span class="font-bold"> lulea hockeyk is the beast logo</span>
+                            <span class="block text-gray-500 text-sm"> long live the kings</span>
+                        </div>
+                        <div class="bg-black-100 text-black-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2"> <span>1977</span></div>
+                    </div>
+                    <div class="m-4">
+                      <div class="bg-white rounded overflow-hidden shadow-md relative">
+                          <img src="https://upload.wikimedia.org/wikipedia/commons/6/6f/20170406_AUTSWE_2919.jpg" alt="" class="w-full h-32 s,: h-48 object-cover">
+                          <div>
+                              <span class="font-bold"> linus omark</span>
+                              <span class="block text-gray-500 text-sm"> nr 67</span>
+                          </div>
+                          <div class="bg-black-100 text-black-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2"> <span>67</span></div>
+                      </div>
+                      <div class="m-4">
+                        <div class="bg-white rounded overflow-hidden shadow-md relative">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Julius_Honka.jpg/375px-Julius_Honka.jpg" alt="" class="w-full h-32 s,: h-48 object-cover">
+                            <div>
+                                <span class="font-bold"> julius honka </span>
+                                <span class="block text-gray-500 text-sm"> the king nr 60</span>
+                            </div>
+                            <div class="bg-black-100 text-black-200 text-xs uppercase font-bold rounded-full p-2 absolute top-0 ml-2 mt-2"> <span>60</span></div>
+                        </div>
+                </div>
+                <h4 class="font-bold mt-12 pb-2 border-b border-black-200" >LHF homepage</h4>
+                <div class="mt-8">
+ 
+                </div>
+                <div class="flex justify-center">
+                    <div class="bg-black-100 text-black-200 uppercase text-xs font-bold cursor-pointer tracking-wider">Load more</div>
+                </div>
+            </div>
+        </main>
+ 
+    </div>
+   
+<style>
+ 
+</style>
+ 
+
